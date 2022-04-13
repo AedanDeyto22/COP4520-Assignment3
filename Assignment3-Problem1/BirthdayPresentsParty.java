074ef2cdf18d6700191ec5be87db26d5cd30dfec
AedanDeyto22 <adeyto722@knights.ucf.edu>
2022-04-13 14:07:59 -0400

    Added Problem 1 & 2

---
+++ Assignment3-Problem1/BirthdayPresentsParty.java
@@ -0,0 +1,346 @@
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.atomic.AtomicMarkableReference;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.TimeUnit;
+
+class Node
+{
+    private int key;
+    AtomicMarkableReference<Node> next = new AtomicMarkableReference<>(null, false);
+
+    Node(int key)
+    {
+        this.key = key;
+    }
+
+    public int getKey()
+    {
+        return key;
+    }
+}
+
+class PresentsList
+{
+    public Node head;
+    public Node tail;
+
+    class Window
+    {
+        public Node pred;
+        public Node curr;
+
+        Window(Node pred, Node curr)
+        {
+            this.pred = pred;
+            this.curr = curr;
+        }
+    } // End of Window
+
+    public Window find(Node head, int key)
+    {
+        Node pred = null;
+        Node curr = null;
+        Node succ = null;
+        boolean [] marked = {false};
+        boolean snip;
+
+        // traverse the List to find where the key might end up at
+        retry : while (true)
+        {
+            // Keep updaing the predicesor and current until we return
+            pred = head;
+            curr = pred.next.getReference();
+
+            while (true)
+            {
+                // Get the reference and marked value of the node after current
+                succ = curr.next.get(marked);
+
+                // If is marked true then try removing current to clean up the thread
+                while (marked[0])
+                {
+                    snip = pred.next.compareAndSet(curr, succ, false, false);
+
+                    if (snip == false) continue retry;
+
+                    curr = succ;
+                    succ = curr.next.get(marked);
+                }
+
+                // When we reach our destination return a new Window class that
+                // hold the addess of the node that should conatin key and its predicesor
+                if (curr.getKey() >= key)
+                {
+                    return new Window(pred, curr);
+                }
+
+                pred = curr;
+                curr = succ;
+            }
+        }
+    } // End of Find
+
+    public boolean add (int key)
+    {
+        while (true)
+        {
+            // First find where the key might be
+            // And then get its predicesor and current node
+            Window window = find(head, key);
+            Node pred = window.pred;
+            Node curr = window.curr;
+
+            // Check to make sure the current node contains key if so
+            // return false to show that key is already in the list
+            if (curr.getKey() == key)
+            {
+                return false;
+            }
+
+            else
+            {
+                // Create a new node for key and sets it tail to the curret node
+                Node node = new Node(key);
+                node.next = new AtomicMarkableReference<>(curr, false);
+
+                // Try adding the new node into the list until its successful
+                if (pred.next.compareAndSet(curr, node, false, false))
+                {
+                    return true;
+                }
+            }
+        }
+    } // End of Add
+
+    public boolean remove(int key)
+    {
+        boolean snip;
+
+        while (true)
+        {
+            // First find where the key might be
+            // And then get its predicesor and current node
+            Window window = find(head, key);
+            Node pred = window.pred;
+            Node curr = window.curr;
+
+            // Check to make sure the current node contains key if not
+            // return false to show that key is not in the list
+            if (curr.getKey() != key)
+            {
+                return false;
+            }
+
+            else
+            {
+                // Get the reference of the node after current.
+                // And marked it to show that being manipulated
+                Node succ = curr.next.getReference();
+                snip = curr.next.compareAndSet(succ, succ, false, true);
+
+                // If failed try again
+                if (!snip)
+                {
+                    continue;
+                }
+
+                // If its able to mark it then perfrom a compare and set and
+                // remove the current node
+                pred.next.compareAndSet(curr, succ, false, false);
+                return true;
+            }
+        }
+    } // End of Remove
+
+    public boolean contains(int key)
+    {
+        boolean [] marked = {false};
+        Node curr = head;
+
+        // We keep traversing until we get where the key might be located
+        // At the same thim we also get the marked value of a node
+        while (curr.getKey() < key)
+        {
+            curr = curr.next.getReference();
+            Node succ = curr.next.get(marked);
+        }
+
+        // If the node is not marked for deletion and it has he key we are looking
+        // for return false, otherwise return false
+        if (curr.getKey() == key && marked[0] == false)
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+    } // End of contains
+}
+
+class Servents implements Runnable
+{
+    private int name;
+    private BirthdayPresentsParty minotour;
+    private PresentsList presents;
+    private boolean canRun;
+    private Random rand = new Random();
+
+    Servents(int name, BirthdayPresentsParty minotour, PresentsList presents)
+    {
+        this.name = name;
+        this.minotour = minotour;
+        this.presents = presents;
+        canRun = true;
+    }
+
+    @Override
+    public void run()
+    {
+        while(canRun)
+        {
+          // First the servent has to choose a job to perforn
+          // It has a 2/5 chance each to either add a gift to the list or remove and create a thank you card
+          // And a 1/5 chance to check for a gift in the list
+          int chooseJob = rand.nextInt(5);
+
+          if (chooseJob <= 1)
+          {
+              // Can only add a gift if we haven't gone above the amount still left
+              if (minotour.getGiftIndex.get() < minotour.presentUnsorted.size())
+              {
+                  // Get a the gift index
+                  int chooseGift = minotour.getGiftIndex.getAndIncrement();
+
+                  // Check to make sure we haven't gone over the index
+                  if (chooseGift < 500000)
+                  {
+                      // Get the gift from the unsorted list and add it into the sorted linked list
+                      int getGift = minotour.presentUnsorted.get(chooseGift);
+                      boolean didItWork = presents.add(getGift);
+
+                      // Increment the size of list so we know we added all 500,000 gifts
+                      int increase = minotour.sizeOfList.getAndIncrement();
+                  }
+              }
+          } // End of Choice 1
+
+          else if (chooseJob > 1 && chooseJob <= 3)
+          {
+              // Remove a present from the List only if there is a gift in the list
+              if (minotour.sizeOfList.get() > 0 && minotour.removeGiftIndex.get() < minotour.sizeOfList.get())
+              {
+                  // We get the index of the gift we are removing
+                  int chooseGift = minotour.removeGiftIndex.getAndIncrement();
+
+                  if (chooseGift < 500000)
+                  {
+                      // We then get the gift ID from the unsortedd list using the index we got
+                      int getGift = minotour.presentUnsorted.get(chooseGift);
+
+                      // We first try to remove the gift
+                      boolean didItWork = presents.remove(getGift);
+
+                      // We might fail to remove it since its in the process of  getting added
+                      // So we keep trying untils it able to remove it
+                      while (presents.contains(getGift) == true || didItWork == false)
+                      {
+                          didItWork = presents.remove(getGift);
+                      }
+
+                      // This represents that we remove the gift from the list and made a thank you card
+                      int decrement = minotour.thankYouCard.getAndIncrement();
+                  }
+              }
+          }
+
+          else
+          {
+              // We get the minotours request by randomly getting a number
+              int minotourRequest = rand.nextInt(500000);
+
+              // And then checking if the present is within the list
+              boolean checkList = presents.contains(minotourRequest);
+          }
+
+          // The thread only stops when all 500000 gift are all sorted and have gotten a thank you card
+          if (minotour.sizeOfList.get() == 500000 && minotour.thankYouCard.get() == 500000)
+          {
+              canRun = false;
+          }
+        } // End of While Loop
+    } // End of run
+}
+
+public class BirthdayPresentsParty
+{
+    public ArrayList<Integer> presentUnsorted = new ArrayList<>();
+    public AtomicInteger sizeOfList = new AtomicInteger(0);
+    public AtomicInteger thankYouCard = new AtomicInteger(0);
+    public AtomicInteger getGiftIndex = new AtomicInteger(0);
+    public AtomicInteger removeGiftIndex = new AtomicInteger(0);
+    public AtomicInteger presentsAmount = new AtomicInteger(500000);
+
+    public static void main(String [] args)
+    {
+        BirthdayPresentsParty minotour = new BirthdayPresentsParty();
+        PresentsList list = new PresentsList();
+
+        // Here we create an ArrayList that will act as our unsorted gift list
+        for (int i = 0; i < 500000; i++)
+        {
+            minotour.presentUnsorted.add(i);
+        }
+
+        // We need to shuffle the list so that we can get an unsorted list;
+        Collections.shuffle(minotour.presentUnsorted);
+
+        // Set up the Linked List
+        list.head = new Node(-1);
+        list.tail = new Node(500001);
+        list.head.next = new AtomicMarkableReference<>(list.tail, false);
+
+        // Starts the timer for the threads
+        long start = System.nanoTime();
+
+        // Create 4 threads
+        Servents servent1 = new Servents(1, minotour, list);
+        Servents servent2 = new Servents(2, minotour, list);
+        Servents servent3 = new Servents(3, minotour, list);
+        Servents servent4 = new Servents(4, minotour, list);
+
+        Thread thread1 = new Thread(servent1);
+        Thread thread2 = new Thread(servent2);
+        Thread thread3 = new Thread(servent3);
+        Thread thread4 = new Thread(servent4);
+
+        // Starts running the 4 Threads
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+
+        // Waits until all Threads are done
+        try
+        {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+            thread4.join();
+        }
+        catch (Exception error)
+        {
+            error.printStackTrace();
+        }
+
+        long end = System.nanoTime();
+        long exectution = end - start;
+        double convert = exectution / 1000000;
+
+        System.out.println("Execution Time: " + convert + " Milliseconds");
+        System.out.println("The amount of Gifts add to the list: " + minotour.sizeOfList.get());
+        System.out.println("The amount of Thank You cards made: " + minotour.thankYouCard.get());
+    }
+}
---
+++ Assignment3-Problem2/TemperatureModule.java
@@ -0,0 +1,385 @@
+import java.util.*;
+import java.io.*;
+import java.util.Collection;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicMarkableReference;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.TimeUnit;
+
+class Node
+{
+    // The Node class
+    private int key;
+    // Is used to tell the size of a list
+    AtomicInteger size = new AtomicInteger(0);
+    AtomicMarkableReference<Node> next = new AtomicMarkableReference<>(null, false);
+
+    Node(int key)
+    {
+        this.key = key;
+    }
+
+    public int getKey()
+    {
+        return key;
+    }
+}
+
+class ReadingList
+{
+    // An ArrayList of Heads of Linked List
+    public List<Node> listHead = new ArrayList<Node>();
+
+    class Window
+    {
+        public Node pred;
+        public Node curr;
+
+        Window(Node pred, Node curr)
+        {
+            this.pred = pred;
+            this.curr = curr;
+        }
+    } // End of Window
+
+    // Exact same function in the BirthdayPresentsParty program
+    public Window find(Node head, int key)
+    {
+        Node pred = null;
+        Node curr = null;
+        Node succ = null;
+        boolean [] marked = {false};
+        boolean snip;
+
+        retry : while (true)
+        {
+            pred = head;
+            curr = pred.next.getReference();
+
+            while (true)
+            {
+                succ = curr.next.get(marked);
+
+                while (marked[0])
+                {
+                    snip = pred.next.compareAndSet(curr, succ, false, false);
+
+                    if (snip == false) continue retry;
+
+                    curr = succ;
+                    succ = curr.next.get(marked);
+                }
+
+                if (curr.getKey() >= key)
+                {
+                    return new Window(pred, curr);
+                }
+
+                pred = curr;
+                curr = succ;
+            }
+        }
+    } // End of Find
+
+    // Edited to allows duplicates and gave it an index parameter
+    // So it knows which list to add in
+    public boolean add (int key, int index)
+    {
+        while (true)
+        {
+            Window window = find(listHead.get(index), key);
+            Node pred = window.pred;
+            Node curr = window.curr;
+
+            Node node = new Node(key);
+            node.next = new AtomicMarkableReference<>(curr, false);
+
+            if (pred.next.compareAndSet(curr, node, false, false))
+            {
+                // If a node is successfully added Increment the size of the list in the head node
+                int increaseSize = listHead.get(index).size.getAndIncrement();
+                return true;
+            }
+        }
+    } // End of Add
+
+    // traverse the list for the next largest value based on key
+    public int traverse(int key)
+    {
+        Node temp = listHead.get(0).next.getReference();
+
+        while (key >= temp.getKey())
+        {
+            // return a -2 to show we are at the end of the list
+            if (temp.next.getReference() == null)
+            {
+                return -2;
+            }
+
+            temp = temp.next.getReference();
+        }
+
+        if (temp.getKey() == 171)
+        {
+            return -2;
+        }
+
+        return temp.getKey();
+    }
+
+    // Gets the max value of a speficied list indicated by index
+    public int getMax(int index)
+    {
+        Node temp = listHead.get(index);
+        int max = -2;
+
+        // Keeps traversing the list until its reachs the tail
+        while (temp.getKey() > max)
+        {
+            max = temp.getKey();
+            temp = temp.next.getReference();
+
+            if (temp.getKey() == 171 || temp.next.getReference() == null)
+            {
+                break;
+            }
+        }
+
+        return max;
+    }
+
+    // Gets the min value of a speficied list indicated by index
+    public int getMin(int index)
+    {
+        Node temp = listHead.get(index);
+        // Just returns the key of the first node after head
+        return temp.next.getReference().getKey();
+    }
+
+    // Returns the size of a specific list
+    public int getSize(int index)
+    {
+        return listHead.get(index).size.get();
+    }
+
+    // Clears the ArrayList of head nodes
+    public void clearList()
+    {
+        listHead.clear();
+    }
+}
+
+class Sensors implements Runnable
+{
+    private TemperatureModule module;
+    private CountDownLatch count;
+    private ReadingList list;
+    private int id;
+    private int job = 0;
+    private boolean canRun;
+    private Random rand = new Random();
+
+    Sensors(int id, TemperatureModule module, CountDownLatch count, ReadingList list)
+    {
+        // Sends the list all the need class its going to interact with
+        this.id = id;
+        this.module = module;
+        this.count = count;
+        this.list = list;
+        canRun = true;
+
+        // Tells the 9 thraed what job it will be performing
+        if (id == 8)
+        {
+            job = 1;
+        }
+    }
+
+    @Override
+    public void run()
+    {
+        if (job == 0)
+        {
+            // For the 8 sensors they will perform 60 reading
+            for (int i = 0; i < 60; i++)
+            {
+                // First get the reading
+                int getReading = rand.nextInt(171);
+
+                // Then add the reading to the main list and the minute list
+                boolean addTemp = list.add(getReading, 0);
+                addTemp = list.add(getReading, i + 1);
+
+                int increaseSize = module.sizeOfList.getAndIncrement();
+            }
+        }
+
+        else
+        {
+            // The list deals with doing the calulations
+            // and i represents the minute we are looking into
+            int i = 0;
+            ArrayList<Integer> listMax = new ArrayList<>();
+            ArrayList<Integer> listMin = new ArrayList<>();
+            ArrayList<Integer> listDiff = new ArrayList<>();
+
+            while(canRun)
+            {
+                // First checks if the Linked list for a ceratin minute is 8 to
+                // show that the minute has passed
+                if (list.getSize(i + 1) == 8)
+                {
+                    // Make sure the array list doesn't exceed size 10 for the 10
+                    // minute intervel. If so remove the head of the list
+                    if (listMax.size() == 10)
+                    {
+                        int remove = listMax.remove(0);
+                        remove = listMin.remove(0);
+                    }
+
+                    // Add the max and min of the minute linked list into
+                    // there respective ArrayList
+                    boolean add = listMax.add(list.getMax(i + 1));
+                    add = listMin.add(list.getMin(i + 1));
+
+                    // Get the max and min value at the currnet momnet based
+                    // on the max and min ArrayList
+                    ArrayList<Integer> temp = new ArrayList<>(listMax);
+                    int size = temp.size();
+                    int max = temp.get(size - 1);
+
+                    temp = new ArrayList<>(listMin);
+                    int min = temp.get(0);
+
+                    // And add the difference into the difference list
+                    add = listDiff.add(max - min);
+
+                    // Then Increment to the next minute
+                    i++;
+                }
+
+                // If we have gone through all 60 minutes then get the results
+                if (i == 60)
+                {
+                    // traverse the diff list to find where the max difference is
+                    // and at what minute
+                    int maxDif = 0;
+                    int time = 0;
+                    for (int j = 0; j < 60; j++)
+                    {
+                        if (maxDif < listDiff.get(j))
+                        {
+                            maxDif = listDiff.get(j);
+                            time = j + 1;
+                        }
+                    }
+
+                    // Then traverse the main list to get the top five min temp
+                    ArrayList<Integer> topMin = new ArrayList<>();
+                    int key = -1;
+
+                    while (topMin.size() != 5)
+                    {
+                        key = list.traverse(key);
+                        topMin.add(key - 100);
+                    }
+
+                    // Then traverse the main list to get the top five max temp
+                    ArrayList<Integer> topMax = new ArrayList<>();
+                    key = -1;
+
+                    while (true)
+                    {
+                        key = list.traverse(key);
+                        if (key == -2)
+                        {
+                            break;
+                        }
+
+                        // Make sure the ArrayList doesn't exceed 5
+                        if (topMax.size() == 5)
+                        {
+                            int remove = topMax.remove(0);
+                        }
+
+                        topMax.add(key - 100);
+                    }
+
+                    // Print the results
+                    System.out.println("The Top 5 Lowest Temp is " + topMin);
+                    System.out.println("The Top 5 Highest Temp is " + topMax);
+                    System.out.println("At time " + time + " has the hightest temp difference of " + (maxDif - 100) + " within a 10 minute intervel.");
+                    canRun = false;
+                }
+            }
+
+        }
+
+        count.countDown();
+    }
+}
+
+public class TemperatureModule
+{
+    AtomicInteger totalReadings = new AtomicInteger(480);
+    AtomicInteger sizeOfList = new AtomicInteger(0);
+
+    public static void main(String [] args)
+    {
+        TemperatureModule module = new TemperatureModule();
+        ReadingList list = new ReadingList();
+        long start = System.nanoTime();
+
+        // represents how many hours we are going to perform the Temperature Reading Module
+        for (int j = 0; j < 10; j++)
+        {
+            CountDownLatch count = new CountDownLatch(9);
+            ExecutorService executor = Executors.newFixedThreadPool(9);
+
+            // Fills the ArrayList in list Reading with the head of a list
+            // Index o represents all reading
+            // Index 1-60 represents the temp reading of the 8 sensor at the current minute
+            for (int i = 0; i < 61; i++)
+            {
+                Node head = new Node(-1);
+                Node tail = new Node(171);
+                head.next = new AtomicMarkableReference<>(tail, false);
+
+                list.listHead.add(head);
+            }
+
+            // Creates all 9 threads
+            // 8 Thrads are the sensor the 9th thraed is the reading thread
+            for (int i = 0; i < 9; i++)
+            {
+                executor.submit(new Sensors(i, module, count, list));
+            }
+
+            executor.shutdown();
+
+            // Waits untill all threds are shutdown
+            try
+            {
+                count.await();
+            }
+            catch (Exception error)
+            {
+                error.printStackTrace();
+            }
+
+            // Resent all values for the next hour
+            list.clearList();
+            module.sizeOfList = new AtomicInteger(0);
+
+            System.out.println("==================== Hour: " + (j + 1) + " ====================");
+        }
+
+        long end = System.nanoTime();
+        long exectution = end - start;
+        double convert = exectution / 1000000;
+
+        System.out.println("Execution Time: " + convert + " Milliseconds");
+    }
+}
